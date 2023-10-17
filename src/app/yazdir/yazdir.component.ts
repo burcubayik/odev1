85d8f5607d7d8276b144ee7ee25814337212d909
Burcu Bayık <burcu.bayik98@gmail.com>
2023-10-17 15:48:00 +0300

    change

--- src/app/yazdir/yazdir.component.ts
+++ src/app/yazdir/yazdir.component.ts
@@ -9,7 +9,7 @@ export class YazdirComponent {
   sayi = 0;
   sayilar: any[]=[]
   yazdir(){
-    for (let index = 1; index < this.sayi; index++) {
+    for (let index = 1; index <= this.sayi; index++) {
       this.sayilar.push(index)
       console.log(index)
     }
